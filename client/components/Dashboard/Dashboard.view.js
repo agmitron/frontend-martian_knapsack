f5eba73d5eb745d9b8184541b7d532a95c826e4e
Alexey <44631357+agmitron@users.noreply.github.com>
2022-10-21 21:38:54 +0800

    wip: keyboard interaction

--- client/components/Dashboard/Dashboard.view.js
+++ client/components/Dashboard/Dashboard.view.js
@@ -1,13 +1,6 @@
-import {
-  arrayOf,
-  bool,
-  func,
-  number,
-  object,
-  oneOf,
-  oneOfType,
-  shape
-} from 'prop-types';
+import { arrayOf, bool, func, number, object, oneOf, shape } from 'prop-types';
+import { useRef } from 'react';
+import { noop } from '../../utils';
 
 import { Button } from '../Button/Button';
 import { CargoCard } from '../CargoCard/CargoCard';
@@ -36,10 +29,63 @@ const propTypes = {
   }).isRequired,
   onAddNewItem: func.isRequired,
   onResetItems: func.isRequired,
-  filter: oneOfType([null, oneOf(['storage', 'cargoHold'])]),
+  filter: oneOf(['storage', 'cargoHold', null]),
   onMoveToCargoHold: func.isRequired,
   onMoveToStorage: func.isRequired
 };
+
+// TODO: think about to move it somewhere outside this file
+function bindKeyboard(keyHandlerMapping) {
+  return e => {
+    const handler = keyHandlerMapping[e.key];
+
+    if (handler) {
+      handler(e);
+    }
+  };
+}
+
+function shiftFocus($current, $next) {
+  const isEdge = !$next;
+
+  if (isEdge) {
+    $current.blur();
+  } else {
+    $next.firstChild.focus();
+  }
+}
+
+const makeColumnShortcuts = ({
+  Enter = noop,
+  ArrowRight = noop,
+  ArrowLeft = noop,
+  ArrowDown = noop,
+  ArrowUp = noop
+}) =>
+  bindKeyboard({
+    ArrowUp: e => {
+      const $current = e.currentTarget;
+      const $prev = $current.previousElementSibling;
+      ArrowUp();
+      shiftFocus($current, $prev);
+    },
+    ArrowDown: e => {
+      const $current = e.currentTarget;
+      const $next = $current.nextElementSibling;
+      ArrowDown();
+      shiftFocus($current, $next);
+    },
+    Enter: e => {
+      const $current = e.currentTarget;
+      const $next = $current.nextElementSibling;
+      const $prev = $current.previousElementSibling;
+      e.preventDefault();
+      Enter();
+      shiftFocus($current, $next || $prev);
+    },
+    ArrowLeft,
+    ArrowRight
+  });
 
 const DashboardView = ({
   cargoHold,
@@ -49,178 +95,151 @@ const DashboardView = ({
   filter,
   onMoveToCargoHold,
   onMoveToStorage
-}) => (
-  <div className={styles.root}>
-    <div className={styles.column}>
-      <SummaryCard
-        title="Storage"
-        imageUrl={storageImageUrl}
-        entries={[
-          { label: 'Value', value: storage.totalValue },
-          { label: 'Weight', value: storage.totalWeight }
-        ]}
-        loading={storage.loading}
-      />
-      <Button
-        variant="outlined"
-        theme="accent"
-        size="md"
-        icon="plus"
-        onClick={onAddNewItem}
-      >
-        Add New Cargo
-      </Button>
+}) => {
+  const storageColumnRef = useRef();
+  const cargoHoldColumnRef = useRef();
 
-      <div className={styles.filters}>
+  return (
+    <div className={styles.root}>
+      <div className={styles.column}>
+        <SummaryCard
+          title="Storage"
+          imageUrl={storageImageUrl}
+          entries={[
+            { label: 'Value', value: storage.totalValue },
+            { label: 'Weight', value: storage.totalWeight }
+          ]}
+          loading={storage.loading}
+        />
         <Button
-          variant="text"
+          variant="outlined"
           theme="accent"
           size="md"
+          icon="plus"
           onClick={onAddNewItem}
-          isActive={filter === 'storage'}
         >
-          Storage
+          Add New Cargo
         </Button>
-        <Button
-          variant="text"
-          theme="accent"
-          size="md"
-          onClick={onAddNewItem}
-          isActive={filter === 'cargoHold'}
-        >
-          Cargo hold
-        </Button>
-      </div>
 
-      <ul className={styles.list}>
-        {storage.loading && <CargoCardSkeleton />}
-        {storage.items.map(item => (
-          <li
-            key={item.id}
-            tabIndex={-1}
-            role="menuitem"
-            onKeyDown={e => {
-              // TODO: Refactor!!!
-              const element = e.currentTarget;
-              switch (e.key) {
-                case 'ArrowLeft': {
-                  break;
-                }
-                case 'ArrowRight': {
-                  break;
-                }
-                case 'ArrowUp': {
-                  const isFirst = !element.previousSibling;
-                  if (!isFirst) {
-                    element.previousElementSibling.firstChild.focus();
-                  } else {
-                    element.blur();
-                  }
-
-                  break;
-                }
-                case 'ArrowDown': {
-                  const isLast = !element.nextSibling;
-                  if (!isLast) {
-                    element.nextElementSibling.firstChild.focus();
-                  } else {
-                    element.blur();
-                  }
-                  break;
-                }
-                case ' ': {
-                  e.preventDefault();
-                  break;
-                }
-                case 'Enter': {
-                  onMoveToCargoHold(item.id);
-                  const isLast = !element.nextSibling;
-                  if (!isLast) {
-                    element.nextElementSibling.firstChild.focus();
-                  } else {
-                    element.blur();
-                  }
-                }
-              }
-            }}
+        <div className={styles.filters}>
+          <Button
+            variant="text"
+            theme="accent"
+            size="md"
+            onClick={onAddNewItem}
+            isActive={filter === 'storage'}
           >
-            <CargoCard
-              title={item.title}
-              description={item.description}
-              imageUrl={item.imageUrl}
-              entries={[
-                { label: 'Value', value: item.value },
-                { label: 'Weight', value: item.weight }
-              ]}
-              actionButton={
-                <Button
-                  variant="outlined"
-                  size="sm"
-                  theme="accent"
-                  onClick={() => onMoveToCargoHold(item.id)}
-                  tabIndex="-1"
-                >
-                  <Icon type="package" />
-                </Button>
-              }
-            />
-          </li>
-        ))}
-      </ul>
+            Storage
+          </Button>
+          <Button
+            variant="text"
+            theme="accent"
+            size="md"
+            onClick={onAddNewItem}
+            isActive={filter === 'cargoHold'}
+          >
+            Cargo hold
+          </Button>
+        </div>
+
+        <ul className={styles.list} ref={storageColumnRef}>
+          {storage.loading && <CargoCardSkeleton />}
+          {storage.items.map(item => (
+            <li
+              key={item.id}
+              tabIndex={-1}
+              role="menuitem"
+              onKeyDown={makeColumnShortcuts({
+                Enter: () => onMoveToCargoHold(item.id),
+                ArrowRight: () =>
+                  cargoHoldColumnRef.current.firstChild.firstChild.focus() // TODO: refactor
+              })}
+            >
+              <CargoCard
+                title={item.title}
+                description={item.description}
+                imageUrl={item.imageUrl}
+                entries={[
+                  { label: 'Value', value: item.value },
+                  { label: 'Weight', value: item.weight }
+                ]}
+                actionButton={
+                  <Button
+                    variant="outlined"
+                    size="sm"
+                    theme="accent"
+                    onClick={() => onMoveToCargoHold(item.id)}
+                    tabIndex="-1"
+                  >
+                    <Icon type="package" />
+                  </Button>
+                }
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={styles.column}>
+        <SummaryCard
+          title="Cargo Hold"
+          imageUrl={cargoHoldImageUrl}
+          entries={[
+            { label: 'Value', value: cargoHold.totalValue },
+            {
+              label: 'Weight',
+              value: cargoHold.totalWeight,
+              limit: cargoHold.weightLimit
+            }
+          ]}
+          loading={cargoHold.loading}
+        />
+        <Button
+          variant="text"
+          theme="alert"
+          size="md"
+          icon="trash"
+          onClick={onResetItems}
+        >
+          Clear All
+        </Button>
+        <ul className={styles.list} ref={cargoHoldColumnRef}>
+          {cargoHold.items.map(item => (
+            <li
+              key={item.id}
+              tabIndex={-1}
+              role="menuitem"
+              onKeyDown={makeColumnShortcuts({
+                Enter: () => onMoveToStorage(item.id),
+                ArrowLeft: () =>
+                  storageColumnRef.current.firstChild.firstChild.focus() // TODO: refactor
+              })}
+            >
+              <CargoCard
+                title={item.title}
+                description={item.description}
+                imageUrl={item.imageUrl}
+                entries={[
+                  { label: 'Value', value: item.value },
+                  { label: 'Weight', value: item.weight }
+                ]}
+                actionButton={
+                  <Button
+                    variant="outlined"
+                    size="sm"
+                    theme="alert"
+                    onClick={() => onMoveToStorage(item.id)}
+                  >
+                    <Icon type="trash" />
+                  </Button>
+                }
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-    <div className={styles.column}>
-      <SummaryCard
-        title="Cargo Hold"
-        imageUrl={cargoHoldImageUrl}
-        entries={[
-          { label: 'Value', value: cargoHold.totalValue },
-          {
-            label: 'Weight',
-            value: cargoHold.totalWeight,
-            limit: cargoHold.weightLimit
-          }
-        ]}
-        loading={cargoHold.loading}
-      />
-      <Button
-        variant="text"
-        theme="alert"
-        size="md"
-        icon="trash"
-        onClick={onResetItems}
-      >
-        Clear All
-      </Button>
-      <ul className={styles.list}>
-        {cargoHold.items.map(item => (
-          <li key={item.id}>
-            <CargoCard
-              title={item.title}
-              description={item.description}
-              imageUrl={item.imageUrl}
-              entries={[
-                { label: 'Value', value: item.value },
-                { label: 'Weight', value: item.weight }
-              ]}
-              actionButton={
-                <Button
-                  variant="outlined"
-                  size="sm"
-                  theme="alert"
-                  onClick={() => onMoveToStorage(item.id)}
-                >
-                  <Icon type="trash" />
-                </Button>
-              }
-              // TODO: refactor
-              onClick={() => onMoveToStorage(item.id)}
-            />
-          </li>
-        ))}
-      </ul>
-    </div>
-  </div>
-);
+  );
+};
 
 DashboardView.propTypes = propTypes;
 
